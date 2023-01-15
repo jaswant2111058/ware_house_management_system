98897aa67e7a6d400cace450a015b243d0579503
jaswant2111058 <jaswant2111058@akgec.ac.in>
2023-01-15 20:23:04 +0530

    agian fixing error

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: "https://teal-tender-squirrel.cyclic.app/auth/google/callback",
   passReqToCallback: true,
 },
 function(request, accessToken, refreshToken, profile, done) {
