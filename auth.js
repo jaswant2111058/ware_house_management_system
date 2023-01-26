5e9aab0714b085df223e9339fb61ac3220722c36
jaswant2111058 <jaswant2111058@akgec.ac.in>
2023-01-26 12:33:31 +0530

    readyfordeploying

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: `http://localhost:${process.env.PORT||5000}/auth/google/callback`,
+  callbackURL: process.env.GOOGLECBURL,
   passReqToCallback: true,
 },
 function(request, accessToken, refreshToken, profile, done) {
